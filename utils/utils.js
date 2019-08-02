64db3d475ed9b48c9c6f36a5b9705dd59ac70f2f
dwyfrequency <jdwy215@gmail.com>
2019-08-02 08:42:12 -0400

    added helper methods

---
+++ utils/utils.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const util = require('util');
+
+// convert readFile to promise so we can use chaining/async await vs. callback
+// string -> promise.resolve(fileData)
+const readFile = fileName => util.promisify(fs.readFile)(fileName, 'utf-8');
+
+// convert array to comma separated string with new line at end
+// array -> string
+const rowToCSV = arr => `${arr.join(',')}\n`;
+
+// capitalize string
+// string -> string
+const capitalize = str => str.toUpperCase();
+
+// invert order of orders
+// [array] -> [array]
+const invertRows = rows => {
+  return rows.reverse();
+};
+
+// (array, [array], bool) -> str
+const stringifyData = (fields, rows, cap = false) => {
+  const header = rowToCSV(fields);
+  const rowStr = rows.map(innerArr => rowToCSV(innerArr)).join('');
+  const returnStr = header + (cap ? capitalize(rowStr) : rowStr);
+  return returnStr;
+};
+
+module.exports = {
+  rowToCSV,
+  capitalize,
+  readFile,
+  invertRows,
+  stringifyData,
+};
